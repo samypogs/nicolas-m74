a0ac5dd10e23a854f412831876aa86605de81076
samypogs <edwardwindtalker@gmail.com>
2019-05-23 08:31:52 +0800

    favicon upload

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -22,21 +22,8 @@ const TemplateWrapper = class extends React.Component {
         <meta name="description" content={this.props.description ? this.props.description : 'Simplify your life'} />
 
         <link
-          rel="apple-touch-icon"
-          sizes="180x180"
-          href="/img/apple-touch-icon.png"
-        />
-        <link
           rel="icon"
-          type="image/png"
-          href="/img/favicon-32x32.png"
-          sizes="32x32"
-        />
-        <link
-          rel="icon"
-          type="image/png"
-          href="/img/favicon-16x16.png"
-          sizes="16x16"
+          href="/img/favicon.ico"
         />
 
         <link
