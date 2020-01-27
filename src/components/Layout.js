047463dcbbc6067a1afb0b934061c691131de8c5
samypogs <edwardwindtalker@gmail.com>
2020-01-27 23:44:16 +0800

    fix capitalization

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -1,7 +1,7 @@
 import React from 'react'
 import Helmet from 'react-helmet'
-import Footer from '../components/Footer'
-import Navbar from '../components/Navbar'
+import Footer from './Footer'
+import Navbar from './Navbar'
 import useSiteMetadata from './SiteMetadata'
 import { Global, css } from "@emotion/core"
 
